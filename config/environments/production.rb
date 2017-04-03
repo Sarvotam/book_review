5837fa30b7821a32f6e0bf807423718d971379bd
Sarvotam <hengajusarvottam@gmail.com>
2017-04-03 16:44:16 +0545

    hait

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,14 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+# Disable Rails's static asset server (Apache or nginx will already do this)
+config.serve_static_assets = false
+
+# Generate digests for assets URLs
+config.assets.digest = true
+
+
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
