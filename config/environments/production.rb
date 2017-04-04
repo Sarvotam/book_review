c6ac466db9f1f92bcbe1584ea219d47d325f26f1
Sarvotam <hengajusarvottam@gmail.com>
2017-04-04 18:23:18 +0545

    final

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,6 @@ config.serve_static_assets = false
 config.assets.digest = true
 
 
-
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
